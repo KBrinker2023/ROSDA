70118167f686e1d33cac2aa92acdf4766c4df4e2
KBrinker2023 <144284209+KBrinker2023@users.noreply.github.com>
2025-03-13 18:20:31 -0400

    Delete MATLAB directory

--- MATLAB/Psi_dx.m
+++
@@ -1,4 +0,0 @@
-function Psi_dx = Psi_dx(z)
-    zeta = 0.11; %Establish tuning parameter
-    Psi_dx = exp(-zeta*z);
-end
--- MATLAB/Theta_0.m
+++
@@ -1,7 +0,0 @@
-function Theta0 = Theta_0(n,W,X,Py,Q)
-    I = eye(n);
-    Pwx = X * inv(X' * W * X) * X' * W;
-    %smallest right singular vectors
-    [~,~,V] = svd((I - Pwx)*W*Py);
-    Theta0 = V(:,end-Q+1:end);
-end
